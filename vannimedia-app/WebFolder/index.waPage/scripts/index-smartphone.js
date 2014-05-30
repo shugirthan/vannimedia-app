09b4b37add126480a3a8e09c2d669076160216ae
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2014-05-30 05:43:46 +0100

    update

---
+++ vannimedia-app/WebFolder/index.waPage/scripts/index-smartphone.js
@@ -0,0 +1,19 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var row1 = {};	// @container
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	row1.click = function row1_click (event)// @startlock
+	{// @endlock
+		// Add your code here
+		$$("navigationView1").goToNextView();
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("row1", "click", row1.click, "WAF");
+// @endregion
+};// @endlock
